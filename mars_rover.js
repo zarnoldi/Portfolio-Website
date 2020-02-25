c3f997aad35bbc45aca52b75ce83fd55c5050271
zarnoldi <thomas@leveret.me>
2020-02-25 21:03:48 +0000

    Movement methods added

--- mars_rover.js
+++ mars_rover.js
@@ -1,30 +1,93 @@
 
 
 
-function Rover(input){
+function Rover(input, IntPostion, Direction){
     this.input = input;
-    this.IntPostion = [0, 0];
-    this.IntDirection = 'N';
-    this.CurrentDirection = '';
-    this.CurrentPostion = [];
-    this.CurrentLocation = '';
-    this.GetCurrentPostionAndDirection = function(input){
+    this.IntPostion = IntPostion;
+    this.Direction = Direction;
+}
 
-        const inputArray = input.str("");
-        const IntX = IntPostion.splice(1,1);
-        const IntY = IntPostion.splice(0,0);
+Rover.prototype.GetCurrentDirection = function(input){
 
-        inputArray.forEach(function(command){
-             if(command = L & CurrentDirection = 'N'){
-                CurrentDirection =
-        });
-        if (command = M) {
+    input.forEach(function(command){
 
-            Rover.CurrenntPostion[0] = Rover.IntPostion + 1
+        // Input L
+        if(command == L && this.Direction == 'N'){
+
+            Direction = 'W';
+
+        }else if(command == L && this.Direction == 'W'){
+            
+            Direction = 'S';
+    
+        }else if (command == L && this.Direction == 'S'){
+
+            Direction = 'E';
+
+        }else if (command == L && this.Direction == 'E'){
+
+            Direction = 'N';
+
+        // Input R
+        } else if (command == R && this.Direction == 'N'){
+            
+            Direction = 'E';
+    
+        }else if (command == R && this.Direction == 'W'){
+
+            Direction = 'N';
+
+        }else if (command == R && this.Direction == 'S'){
+
+            Direction = 'W';
+
+        }else if (command == R && this.Direction == 'E'){
+
+            Direction = 'S';
+        }else {
+
+            console.log('Incorrect input');
+        }
     };
-    return 
+ return this.Direction;
+};
+
+Rover.prototype.GetCurrentPostion = function(input){
+
+    input.forEach(function(command){
+
+        if (command == M && this.Direction == N){
+
+            Xcordindate = this.IntPostion[0];
+
+            Ycordindate = this.IntPostion[1];
+
+            NewYPostion = Ycordindate + 1;
+
+            currentPostion = [Xcordindate, NewYPostion];
+
+        } else if ()
+
+
+    });
+
+
+
+
+
+
+return currentPostion
 }
-}
+
+
+
+
+
+
+
+
+
+
 
 const rover1 = new Rover('LMR');
 
