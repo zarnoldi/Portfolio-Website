6d2f3b9844da582292f8ecd4475a8b7c2272494a
zarnoldi <thomas@leveret.me>
2020-02-25 21:01:40 +0000

    updated Mars rover work

---
+++ mars_rover.js
@@ -0,0 +1,31 @@
+
+
+
+function Rover(input){
+    this.input = input;
+    this.IntPostion = [0, 0];
+    this.IntDirection = 'N';
+    this.CurrentDirection = '';
+    this.CurrentPostion = [];
+    this.CurrentLocation = '';
+    this.GetCurrentPostionAndDirection = function(input){
+
+        const inputArray = input.str("");
+        const IntX = IntPostion.splice(1,1);
+        const IntY = IntPostion.splice(0,0);
+
+        inputArray.forEach(function(command){
+             if(command = L & CurrentDirection = 'N'){
+                CurrentDirection =
+        });
+        if (command = M) {
+
+            Rover.CurrenntPostion[0] = Rover.IntPostion + 1
+    };
+    return 
+}
+}
+
+const rover1 = new Rover('LMR');
+
+console.log(rover1);
