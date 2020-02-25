c5b08726ff588afdf405fee044ff50ceb49e5e84
zarnoldi <thomas@leveret.me>
2020-02-25 21:24:27 +0000

    Latest mars rover

--- mars_rover.js
+++ mars_rover.js
@@ -12,76 +12,87 @@ Rover.prototype.GetCurrentDirection = function(input){
     input.forEach(function(command){
 
         // Input L
-        if(command == L && this.Direction == 'N'){
+        if(command == 'L' && this.Direction == 'N'){
 
             Direction = 'W';
 
-        }else if(command == L && this.Direction == 'W'){
+        }else if(command == 'L' && this.Direction == 'W'){
             
             Direction = 'S';
     
-        }else if (command == L && this.Direction == 'S'){
+        }else if (command == 'L' && this.Direction == 'S'){
 
             Direction = 'E';
 
-        }else if (command == L && this.Direction == 'E'){
+        }else if (command == 'L' && this.Direction == 'E'){
 
             Direction = 'N';
 
         // Input R
-        } else if (command == R && this.Direction == 'N'){
+        } else if (command == 'R' && this.Direction == 'N'){
             
             Direction = 'E';
     
-        }else if (command == R && this.Direction == 'W'){
+        }else if (command == 'R' && this.Direction == 'W'){
 
             Direction = 'N';
 
-        }else if (command == R && this.Direction == 'S'){
+        }else if (command == 'R' && this.Direction == 'S'){
 
             Direction = 'W';
 
-        }else if (command == R && this.Direction == 'E'){
+        }else if (command == 'R' && this.Direction == 'E'){
 
             Direction = 'S';
         }else {
 
             console.log('Incorrect input');
         }
-    };
+    });
  return this.Direction;
 };
 
 Rover.prototype.GetCurrentPostion = function(input){
 
+    Xcordindate = this.IntPostion[0];
+    Ycordindate = this.IntPostion[1];
+
     input.forEach(function(command){
 
-        if (command == M && this.Direction == N){
-
-            Xcordindate = this.IntPostion[0];
-
-            Ycordindate = this.IntPostion[1];
+        if (command == 'M' && this.Direction == 'N'){
 
             NewYPostion = Ycordindate + 1;
 
             currentPostion = [Xcordindate, NewYPostion];
 
-        } else if ()
+        } else if (command == 'M' && this.Direction == 'E'){
+
+            NewXPostion = Xcordindate + 1;
+
+            currentPostion = [NewXPostion, Ycordindate];
+
+        }else if (command == 'M' && this.Direction == 'S'){
+
+            NewYPostion = Ycordindate - 1;
+
+            currentPostion = [Xcordindate, NewYPostion];
+    
+        }else if (command == 'M' && this.Direction == 'W'){
+
+            NewXPostion = Xcordindate -1;
+
+            currentPostion = [NewXPostion, Ycordindate];
+
+        }else {
+
+            console.log('Incorrect Input');
+        }
 
 
     });
 
-
-
-
-
-
-return currentPostion
+return currentPostion;
 }
-
-
-
-
 
 
 
